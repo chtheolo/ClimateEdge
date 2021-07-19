9706418a56f00f49888423ebb7b86d2519bf0e54
chtheolo <chtheolo@gmail.com>
2021-07-19 15:01:34 +0300

    Move all inside functions into seperate files.

--- api_r_w.js
+++ api_r_w.js
@@ -1,26 +1,10 @@
-const { createReadStream , createWriteStream } = require('fs');
+const { createReadStream } = require('fs');
 const { pipeline } = require('stream');
-const util = require('util');
 const EventEmitter = require('events');
-const { Pool } = require('pg');
-const dotenv = require('dotenv');
 const { exit } = require('process');
-dotenv.config();
-
-/** POOL CONNECTION TO POSTGRESQL **/
-const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  host: 'localhost',
-  database: 'interviewdb',
-  password: process.env.PASSWORD,
-  port: process.env.PORT,
-})
-
-pool.on('error', (err, client) => {
-	console.error('Unexpected error on idle client', err)
-	writeLogOutput(err);
-	process.exit(-1)
-})
+const pool = require('./dbQuery');
+const json = require('./writeJson');
+const log = require('./writeLog');
 
 /** BUFFERS **/
 var buffer_str= ''; 
@@ -53,10 +37,6 @@ else if (filename != undefined && filename.length >=0 && filename.endsWith('.jso
 	process.exit(-1);
 }
 
-/** Create WriteStream **/
-const write_json_stream = createWriteStream('output.json');
-const write_log_stream = createWriteStream('output.log', { flags: 'a' });
-
 /*------- Given functions ---------*/
 const getRandomInt = (min, max) => {
     min = Math.ceil(min);
@@ -86,84 +66,6 @@ async function pump() {
 			buffer_obj.push(JSON.parse(line));
 		}
 		buffer_str = buffer_str.slice(pos+1); 		// remove the line from our buffer
-	}
-}
-
-
-/** This function writes our logs into out output log file. */
-function writeLogOutput(logs) {
-	write_log_stream.write(util.format('%s\n',new Date().toUTCString()));
-	write_log_stream.write(util.format('%s\n', logs));
-}
-
-/** This function is responsible to create a thread pool 
- * 	and save our data to our postgresql DB. */
-function post(statistics) {
-	pool.connect((error, client, release) => {
-		if (error) {
-			writeLogOutput(error);
-		}
-		else {
-			let columns = ''
-			let values = '';
-			let i=1;
-			let val_arr = [];
-			let length = Object.keys(statistics).length;
-			for (let key in statistics) {
-				if (i == length) {
-					columns = columns.concat(key);
-					values = values.concat(`$${i}`);
-				}
-				else {
-					columns  = columns.concat(key + ', ');
- 		           values = values.concat(`$${i}, `);
-				}
-				val_arr.push(statistics[key]);
-				i++;
-			}
-
-			client.query('INSERT INTO report(' + columns + ') VALUES (' + values + ')', val_arr, (error, results) => {
-				release();
-				if (error) {
-					console.error(error);
-					writeLogOutput(error);
-				}
-				else {
-					writeLogOutput('Insert query sucessfully saved to DB!');
-				}
-			});
-		}
-	});
-}
-
-/** A function that writes the output into a json file. */
-function writeJSONOutput(writeLogOutput) {
-	time_reference = process.hrtime();
-
-	buffer_str = JSON.stringify(buffer_obj);
-	var pos;
-	var line;
-
-	pos = buffer_str.indexOf('[');
-	line = buffer_str.slice(0,pos+1);
-	write_json_stream.write(util.format('%s\n',line));
-
-	buffer_str = buffer_str.slice(pos+1);			// remove line from buffer
-	while((pos = buffer_str.indexOf('}')) >= 0) {
-		if (buffer_str[pos+1] == ',') {
-			line = buffer_str.slice(0, pos+1);
-			write_json_stream.write(util.format('\t%s,\n',line));
-		}
-		else {
-			line = buffer_str.slice(0, pos+1);
-			write_json_stream.write(util.format('\t%s\n]\n',line));
-			// --> timestamp for Writing time
-			statistics.write_execution_time = process.hrtime(time_reference)[1]/1000000;
-			buffer_obj = []; 						// empty our buffer
-			writeLogOutput(statistics);
-			post(statistics);						// Save data to DB
-		}
-		buffer_str = buffer_str.slice(pos+2);		// remove line from buffer
 	}
 }
 
@@ -207,13 +109,37 @@ pipeline(
 	(error) => {
 		if (error) {
 			statistics.pipeline_errors_count++;
-			writeLogOutput(error);
+			log.writeLogOutput(error);
 		}
 		else {
 			console.log('Pipeline succeeded!')
-			myAPIemitter.on('event', function() {
-				writeJSONOutput(writeLogOutput);
+			myAPIemitter.on('event', async function() {
+				buffer_str = JSON.stringify(buffer_obj);
+
+				try {
+					var res = await json.writeJSONOutput(buffer_obj);
+					statistics.write_execution_time = res.time;
+
+					buffer_obj = []; 								// empty the buffer
+					log.writeLogOutput(statistics);					// Write the data in .log
+					log.writeLogOutput(res.message);
+					try{
+						var db = await pool.post(statistics);		// Save data to DB
+						log.writeLogOutput(db);
+					}
+					catch(error) {
+						log.writeLogOutput(error);
+					}
+				} catch (error) {
+					log.writeLogOutput(error);
+				}
 			});
 		}
 	}
 );
+
+module.exports = {
+	pump: "pump",
+	post: "post",
+	writeJSON: "writeJSONOutput"
+};
